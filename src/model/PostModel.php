6383ea612010ae59f7aea919bacbc7a656225ba3
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-09-27 17:14:24 +0700

    fix: modify setPostText to updatePost

--- src/model/PostModel.php
+++ src/model/PostModel.php
@@ -3,11 +3,12 @@
 require_once PROJECT_ROOT_PATH . "./src/bases/PostModel.php";
 
 class PostModel extends BaseModel {
-    public $post_id;
-    public $post_text;
-    public $like_count;
-    public $post_time;
-    // public $last_modify_time;
+    private $post_id;
+    private $post_text;
+    private $like_count;
+    private $post_time;
+    // private $last_modify_time;
+    private $resource_id;
 
     public function __construct() {
         $this->_primary_key = 'user_id';
@@ -15,21 +16,34 @@ class PostModel extends BaseModel {
     }
 
     public function constructFromArray($array) {
-        $this->post_id    = $array['post_id'];
-        $this->post_text  = $array['post_text'];
-        $this->like_count = $array['like_count'];
-        $this->post_time  = $array['post_time'];
+        $this->post_id          = $array['post_id'];
+        $this->post_text        = $array['post_text'];
+        $this->like_count       = $array['like_count'];
+        $this->post_time        = $array['current_time'];
+        // $this->last_modify_time = $array['current_time'];
+        $this->resource_id      = $array['resource_id'];
 
         return $this;
     }
 
-    public function getPostID() { return $this->post_id; }
-    public function getPostText() { return $this->post_text; }
-    public function getLikeCount() { return $this->like_count; }
-    public function getPostTime() { return $this->post_time; }
+    public function updatePost($array) {
+        if ($array['post_text'] != '') {
+            $this->post_text = $array['post_text'];
+        }
+        // if ($array['time'] != '') {
+        //     $this->last_modify_time = $array['time'];
+        // }
+        if ($array['resource_id'] != ['']) {
+            $this->resource_id = $array['resource_id'];
+        }
+    }
 
-    public function setPostText($text) { $this->post_text = $text; }
-    public function like() { $this->like_count++; }
-    public function unlike() { $this->like_count--; }
+    public function getPostID()    { return $this->post_id; }
+    public function getPostText()  { return $this->post_text; }
+    public function getLikeCount() { return $this->like_count; }
+    public function getPostTime()  { return $this->post_time; }
+
+    public function like()             { $this->like_count++; }
+    public function unlike()           { $this->like_count--; }
 }
 >
