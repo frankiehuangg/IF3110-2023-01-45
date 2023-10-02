caa1c44d6b7a84e0cd424bf260ff565cbf239644
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-10-02 13:09:46 +0700

    fix: post card not showing

--- public/view/home.php
+++ public/view/home.php
@@ -7,7 +7,7 @@ $postService = PostService::getInstance();
 $posts = $postService->getNLastPosts(10);
 
 usort($posts, function($a, $b) {
-    return $a->getPostTime() > $b->getPostTime();
+    return $a->getPostTimestamp() > $b->getPostTimestamp();
 });
 
 $postElmts = implode('', array_map(function($post) {
@@ -15,7 +15,6 @@ $postElmts = implode('', array_map(function($post) {
 }, $posts));
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
