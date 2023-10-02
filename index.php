0cc4013330f96fdac5b38a20f64d70f961277c98
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-10-02 09:04:42 +0700

    style: missing php end tag

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-define("PROJECT_ROOT_PATH", __DIR__ . '/.');
+define("PROJECT_ROOT_PATH", __DIR__ . '/');
 
-require_once PROJECT_ROOT_PATH . '/src/router/PageRouter.php';
-require_once PROJECT_ROOT_PATH . '/routes.php';
+require_once PROJECT_ROOT_PATH . 'src/router/PageRouter.php';
+require_once PROJECT_ROOT_PATH . 'routes.php';
 
 $router = new PageRouter($routes);
-$router->setErrorRoute(PROJECT_ROOT_PATH . '/public/view/404.php');
+$router->setErrorRoute(PROJECT_ROOT_PATH . 'public/view/404.php');
 $router->routing($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+
+?>
