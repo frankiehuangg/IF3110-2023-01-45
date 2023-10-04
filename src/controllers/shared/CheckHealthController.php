ad819b19695187bad8ab37f74819d49951a2d172
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-10-04 18:30:10 +0700

    feat: CheckHealthController intial commit

---
+++ src/controllers/shared/CheckHealthController.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once PROJECT_ROOT_PATH . '/src/bases/BaseController.php';
+
+class CheckHealthController extends BaseController {
+    protected static $instance;
+
+    private function __construct($service) {
+        parent::__construct($service);
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new static(null);
+        }
+
+        return self::$instance;
+    }
+
+    public function post($urlParams) {
+        return json_encode($_POST);
+    }
+
+    public function get($urlParams) {
+        return 'OK';
+    }
+}
+
+?>
