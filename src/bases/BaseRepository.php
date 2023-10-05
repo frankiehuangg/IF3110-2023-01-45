13e60c3931bef3b8ce53e3613df621feec705da4
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-10-05 14:07:23 +0700

    fix: string concat to internal variable

--- src/bases/BaseRepository.php
+++ src/bases/BaseRepository.php
@@ -24,24 +24,24 @@ abstract class BaseRepository {
     }
 
     public function getAll() {
-        $sql = 'SELECT * FROM ' . $this->tableName;
+        $sql = "SELECT * FROM ' $this->tableName";
         return $this->pdo->query($sql);
     }
 
     public function countRow($where = []) {
-        $sql = 'SELECT COUNT(*) FROM ' . $this->tableName;
+        $sql = "SELECT COUNT(*) FROM $this->tableName";
 
         $WHERE_CASE_EXIST = (count($where) > 0);
         if ($WHERE_CASE_EXIST) {
-            $sql = $sql . ' WHERE ';
+            $sql = $sql . " WHERE ";
 
-            $sql = $sql . implode(' AND ', array_map(function($key, $val) {
+            $sql = $sql . implode(" AND ", array_map(function($key, $val) {
                 $LIKE_CASE_EXIST = ($val[0] === 1);
                 if ($LIKE_CASE_EXIST) {
-                    return '$key LIKE :$key';
+                    return "$key LIKE :$key";
                 }
 
-                return '$key = :$key';
+                return "$key = :$key";
             }, array_keys($where), array_values($where)));
         }
 
@@ -50,13 +50,11 @@ abstract class BaseRepository {
             $VALUE = htmlspecialchars($val[1]);
             $TYPE  = htmlspecialchars($val[2]);
 
-            # $stmt->bindValue(':$key', $VALUE, $TYPE);
-
             $LIKE_CASE_EXIST = ($val[0] === 1);
             if ($LIKE_CASE_EXIST) {
-                $stmt->bindValue(':$key', '%$VALUE%', $TYPE);
+                $stmt->bindValue(":$key", "%$VALUE%", $TYPE);
             } else {
-                $stmt->bindValue(':$key', $VALUE, $TYPE);
+                $stmt->bindValue(":$key", $VALUE, $TYPE);
             }
         }
 
@@ -71,24 +69,26 @@ abstract class BaseRepository {
         $pageNo   = null,
         $pageSize = null
     ) {
-        $sql = 'SELECT * FROM ' . $this->tableName;
+        $sql = "SELECT * FROM $this->tableName";
 
         $WHERE_CASE_EXIST = (count($where) > 0);
         if ($WHERE_CASE_EXIST) {
-            $sql = $sql . ' WHERE ';
+            $sql = $sql . " WHERE ";
             $sql = $sql . implode(' AND ', array_map(function($key, $val) {
                 $LIKE_CASE_EXIST = ($val[0] === 1);
                 if ($LIKE_CASE_EXIST) {
-                    return $key . ' LIKE :' . $key;
+                    return "$key LIKE :$key";
                 }
 
-                return $key . ' = :' . $key;
+                return "$key = :$key";
             }, array_keys($where), array_values($where)));
         }
 
         $ORDER_BY_CASE_EXIST = ($orderBy !== '');
         if ($ORDER_BY_CASE_EXIST) {
-            $sql = $sql . ' ORDER BY ' . htmlspecialchars($orderBy);
+            $ORDER_BY = htmlspecialchars($orderBy);
+
+            $sql = $sql . " ORDER BY $ORDER_BY";
         }
 
         $SORT_DESC_CASE = ($sortRes === 1);
@@ -108,7 +108,7 @@ abstract class BaseRepository {
 
             $LIKE_CASE_EXIST = ($val[0] === 1);
             if ($LIKE_CASE_EXIST) {
-                $VALUE = '%' . $VALUE . '%';
+                $VALUE = "%$VALUE$";
                 $stmt->bindValue(':' . $key, $VALUE, $TYPE);
             } else {
                 $stmt->bindValue(':' . $key, $VALUE, $TYPE);
@@ -186,24 +186,29 @@ abstract class BaseRepository {
     }
 
     public function update($model, $params) {
-        $sql = 'UPDATE ' . $this->tableName . ' SET $key = :$key';
+        $sql = "UPDATE $this->tableName SET ";
 
-        for ($i = 0; $i < count($params) - 1; $i++) {
-            $sql = $sql . ', $key = :$key';
+        $i = 0;
+        foreach ($params as $key => $val) {
+            $sql = "$key = :$key";
+
+            if ($i < count($params) - 1) {
+                $sql = $sql . '. ';
+            }
+
+            $i++;
         }
 
         $primaryKey = $model->get('_primary_key');
-
-        $sql = $sql . ' WHERE $primaryKey = :primaryKey';
+        $sql = $sql . " WHERE $primaryKey = :primaryKey";
 
         $stmt = $this->pdo->prepare($sql);
         foreach ($params as $key => $val) {
-            $VALUE = $model->get(htmlspecialchars($key));
-            $TYPE  = htmlspecialchars($val);
-
-            $stmt->bindValue(':$key', $VALUE, $TYPE);
+            $VALUE = htmlspecialchars($model->get($key));
+            $TYPE  = $val;
+            $stmt->bindValue(":$key", $VALUE, $TYPE);
         }
-        $stmt->bindValue(':primaryKey', $model->get($primaryKey), PDO::PARAM_INT);
+        $stmt->bindValue(":primaryKey", $model->get($primary_key), PDO::PARAM_INT);
 
         $stmt->execute();
         return $stmt->rowCount();
@@ -212,7 +217,7 @@ abstract class BaseRepository {
     public function delete($model) {
         $primaryKey = $model->get('_primary_key');
 
-        $sql = 'DELETE FROM ' . $this->tableName . ' WHERE $primaryKey = :primaryKey';
+        $sql = "DELETE FROM $this->tableName WHERE $primaryKey = :primaryKey";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':primaryKey', $model->get(htmlspecialchars($primaryKey)), PDO::PARAM_INT);
@@ -222,7 +227,7 @@ abstract class BaseRepository {
     }
 
     public function getNLastRow($n) {
-        $sql = 'SELECT COUNT(*) FROM ' . $this->tableName;
+        $sql = "SELECT COUNT(*) FROM $this->tableName";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute();
@@ -235,7 +240,7 @@ abstract class BaseRepository {
 
         $offset = $count - $n;
 
-        $sql = 'SELECT * FROM ' . $this->tableName . ' LIMIT :limit OFFSET :offset';
+        $sql = "SELECT * FROM $this->tableName LIMIT :limit OFFSET :offset";
 
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':limit', $n, PDO::PARAM_INT);
