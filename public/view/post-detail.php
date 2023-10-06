f2ce04171e6d69d0317bffe6dc2fa8ae57cf7c21
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-10-06 14:03:45 +0700

    feat: post detail page initial commit

---
+++ public/view/post-detail.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial=scale=1.0">
+        <link rel="stylesheet" href="/public/css/global.css">
+        <link rel="stylesheet" href="/public/css/home.css">
+        <title>Create A Post</title>
+    </head>
+    <body>
+    <div class="form-container">
+        <div class="page d-flex">
+            <div id="main">
+                <div id="content">
+                    <div class="post-list d-flex flex-wrap" id="post-list">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script defer async src="/public/js/lib.js"></script>
+        <script defer async src="/public/js/post-detail.js"></script>
+    </body>
+</html>
